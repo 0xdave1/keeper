c12424dec6417b4c93659dd7a6a0fd956071cbf9
0xdave1 <josephziondavid@gmail.com>
2025-07-20 16:56:20 +0100

    note updated

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+if (process.env.NODE_ENV !== "production") {
+  console.warn("Note component is being rendered");
+}
+
 function Note() {
   return <div className="note">
     <h1>Title</h1>
