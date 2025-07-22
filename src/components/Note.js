f48a3e993d418d1ece304d1f97069a73f08e4e18
0xdave1 <josephziondavid@gmail.com>
2025-07-22 20:17:11 +0100

    something up

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,7 +4,10 @@ import React from "react";
 function Note() {
   return <div className="note">
     <h1>Title</h1>
-    <p>This is a note.</p>
+    <p>This is a note.</p>  
+    <words>Words</words>
+    <p>More text here.</p>
+   
   </div>
 }
 
